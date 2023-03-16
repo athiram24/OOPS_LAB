b036f3498ce26d3ba88bab93d2b7e993635680d1
Athiram24 <2242@tkmce.ac.in>
2023-03-16 06:10:54 +0530

    co2updated

---
+++ CO2/main.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+import java.util.Arrays;
+class sort_str 
+{
+
+
+
+}
+class main
+{
+    public static void main(String args[])
+    {
+
+        Scanner inp = new Scanner(System.in);
+        System.out.println("Enter a string:");
+        String str = inp.nextLine();
+        System.out.println("Before sorting:" + str);
+        sort_str obj = new sort_str(str);
+        System.out.println("After sorting:" + );
+
+
+
+
+    }
+}
